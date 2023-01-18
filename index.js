e0d3947abaf224f43472499381aea2068ca0ed66
cellva03 <selvavignesh432@gmail.com>
2023-01-18 13:37:30 +0530

    environment var

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const blogRoutes = require('./routes/blogRoutes')
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 // const Blog = require('./models/blog.js');
 // const serverless = require('serverless-http');
 
 const app = express();
+dotenv.config()
 const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true}));
 
-const dbURI = 'mongodb://Selva:Selva03@blog-shard-00-00.5ihud.mongodb.net:27017,blog-shard-00-01.5ihud.mongodb.net:27017,blog-shard-00-02.5ihud.mongodb.net:27017/selva-blog?ssl=true&replicaSet=atlas-pi94wt-shard-0&authSource=admin&retryWrites=true&w=majority';
+const dbURI = process.env.DBURL;
 mongoose.connect(dbURI)
     .then(()=>{
         console.log('DataBase Connected');
